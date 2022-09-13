a373b3aa9e818d5e65b62ad304bad3cc52548d10
leximainw <leximainw@gmail.com>
2022-09-13 12:03:53 -0400

    condense App component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import { filmData } from './film-data'
 import FilmDetail from './FilmDetail'
 import Slideshow from './Slideshow'
 
-function App() {
-    return (
-        <Slideshow
-            slides={filmData.map(film => <FilmDetail film={film} />)}
-        />
-    )
-}
+const App = () => <Slideshow slides={filmData.map(film => <FilmDetail film={film} />)}/>
 
 export default App

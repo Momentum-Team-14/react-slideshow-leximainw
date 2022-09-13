b21dbbc78c948c7f2ddff993bb9bf12d7fd3f93d
leximainw <leximainw@gmail.com>
2022-09-13 12:43:04 -0400

    use local film data for testing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import './App.css'
-// import { filmData } from './film-data'
+import { filmData } from './film-data'
 // import { ghibliData } from './ghibli-data'
 // const filmData = ghibliData
 import FilmDetail from './FilmDetail'
 import Slideshow from './Slideshow'
-import {useEffect, useState} from 'react'
+// import {useEffect, useState} from 'react'
 
 const App = () => {
-    const [filmData, setFilmData] = useState([])
-    useEffect(() => {
-        fetch('https://ghibliapi.herokuapp.com/films/')
-            .then(res => res.json())
-            .then(data => setFilmData(data))
-            .catch(err => {
-                console.log('Error fetching film data:', err)
-                setFilmData(undefined)
-            })
-    })
-    if (filmData === undefined) {
-        return (
-            <>
-                <h1 style={{textAlign: 'center'}}>Couldn't load film data</h1>
-                <div style={{display: 'flex', justifyContent: 'center'}}>
-                    <button className='btn btn-info' onClick={() => setFilmData([])}>Retry?</button>
-                </div>
-            </>
-        )
-    }
+    // const [filmData, setFilmData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://ghibliapi.herokuapp.com/films/')
+    //         .then(res => res.json())
+    //         .then(data => setFilmData(data))
+    //         .catch(err => {
+    //             console.log('Error fetching film data:', err)
+    //             setFilmData(undefined)
+    //         })
+    // })
+    // if (filmData === undefined) {
+    //     return (
+    //         <>
+    //             <h1 style={{textAlign: 'center'}}>Couldn't load film data</h1>
+    //             <div style={{display: 'flex', justifyContent: 'center'}}>
+    //                 <button className='btn btn-info' onClick={() => setFilmData([])}>Retry?</button>
+    //             </div>
+    //         </>
+    //     )
+    // }
 
     return (
         <Slideshow

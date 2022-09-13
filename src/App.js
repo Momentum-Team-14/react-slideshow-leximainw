bc78861e9f112cf1aaf1d52ce72292993d8d40c2
leximainw <leximainw@gmail.com>
2022-09-13 13:09:21 -0400

    feat: re-enable all spicy code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import './App.css'
-import { filmData } from './film-data'
+// import { filmData } from './film-data'
 // import { ghibliData } from './ghibli-data'
 // const filmData = ghibliData
 import FilmDetail from './FilmDetail'
 import Slideshow from './Slideshow'
-import {useState} from 'react'
-// import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 
 const App = () => {
-    // const [filmData, setFilmData] = useState([])
-    // useEffect(() => {
-    //     fetch('https://ghibliapi.herokuapp.com/films/')
-    //         .then(res => res.json())
-    //         .then(data => setFilmData(data))
-    //         .catch(err => {
-    //             console.log('Error fetching film data:', err)
-    //             setFilmData(undefined)
-    //         })
-    // })
-    // if (filmData === undefined) {
-    //     return (
-    //         <>
-    //             <h1 style={{textAlign: 'center'}}>Couldn't load film data</h1>
-    //             <div style={{display: 'flex', justifyContent: 'center'}}>
-    //                 <button className='btn btn-info' onClick={() => setFilmData([])}>Retry?</button>
-    //             </div>
-    //         </>
-    //     )
-    // }
-
+    const [filmData, setFilmData] = useState([])
+    useEffect(() => {
+        fetch('https://ghibliapi.herokuapp.com/films/')
+            .then(res => res.json())
+            .then(data => setFilmData(data))
+            .catch(err => {
+                console.log('Error fetching film data:', err)
+                setFilmData(undefined)
+            })
+    })
     const [sortMode, setSortMode] = useState(0)
+    if (filmData === undefined) {
+        return (
+            <>
+                <h1 style={{textAlign: 'center'}}>Couldn't load film data</h1>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <button className='btn btn-info' onClick={() => setFilmData([])}>Retry?</button>
+                </div>
+            </>
+        )
+    }
 
     const sortKinds = [
         {
@@ -57,7 +55,7 @@ const App = () => {
 
     return (
         <Slideshow
-            /*pageSize={10}*/
+            pageSize={10}
             slides={[...filmData]
                 .sort(sortKinds[sortMode].fn)
                 .map(film => <FilmDetail key={film.title} film={film}/>)}

3bce22a4934c9eb869cdeb58e06fbfed977c330b
leximainw <leximainw@gmail.com>
2022-09-13 13:02:32 -0400

    clean up Slideshow component

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -1,17 +1,18 @@
 import {useState} from 'react'
 
-const Slideshow = ({slides, pageSize=1, sortBy='', onSort=undefined}) => {
+const Slideshow = props => {
+    const slides = props.slides
     const btnClasses = "btn btn-primary"
     const btnDisabled = btnClasses + " disabled"
     const [currPage, setCurrSlide] = useState(0)
-    const currIndex = currPage * pageSize
-    const numPages = Math.ceil(slides.length / pageSize)
-    if (!onSort) {
-        onSort = ''
+    const currIndex = currPage * props.pageSize
+    const numPages = Math.ceil(slides.length / props.pageSize)
+    if (!props.onSort) {
+        props.onSort = ''
     }
     return (
         <div className="slides-app">
-            {(numPages ? numPages : '') && slides.slice(currIndex, currIndex + pageSize)}
+            {(numPages ? numPages : '') && slides.slice(currIndex, currIndex + props.pageSize)}
             <div style={{display: 'flex', justifyContent: 'center', marginTop: '1rem'}}>
                 <div className="btn-group">
                     <button
@@ -26,10 +27,10 @@ const Slideshow = ({slides, pageSize=1, sortBy='', onSort=undefined}) => {
                         className={currPage < numPages - 1 ? btnClasses : btnDisabled}
                         onClick={() => setCurrSlide(currPage + 1)}
                     >Next</button>
-                    {onSort && <button
+                    {props.onSort && <button
                         className={btnClasses}
-                        onClick={onSort}
-                    >Sort{sortBy ? ` by ${sortBy}` : ''}</button>}
+                        onClick={props.onSort}
+                    >{props.sortText ? props.sortText : props.sortBy ? `Sort by ${props.sortBy}` : 'Sort'}</button>}
                 </div>
             </div>
         </div>

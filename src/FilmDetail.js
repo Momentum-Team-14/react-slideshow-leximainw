3dc2a92fdd62201b2f1c85fbac48ae45a09e2ef4
leximainw <leximainw@gmail.com>
2022-09-13 14:35:06 -0400

    feat: add style to site

--- src/FilmDetail.js
+++ src/FilmDetail.js
@@ -1,7 +1,7 @@
 const FilmDetail = ({film}) => {
     return (
-        <div style={{display: 'flex', width: '100vw'}}>
-            <div style={{maxWidth: '15%', marginRight: '1rem'}}><img src={film.image} style={{width: '100%'}} alt={`Film cover for {currFilm.title}`}></img></div>
+        <div style={{display: 'flex', width: '100%'}}>
+            <div style={{maxWidth: '20%', marginRight: '1rem', flexShrink: 0}}><img src={film.image} style={{width: '100%'}} alt={`Film cover for {currFilm.title}`}></img></div>
                 <div>
                 <div><span title={film.original_title_romanised}>{film.original_title}</span> <span>({film.title})</span></div>
                 <div><span style={{marginRight: '1rem'}}>Released {film.release_date}</span><span style={{marginRight: '1rem'}}>Runtime: {film.running_time} minutes</span><span>Rotten Tomatoes score: {film.rt_score}%</span></div>

41fb62f15140ac130a3a7a59a3bf6b2cc9dfc787
leximainw <leximainw@gmail.com>
2022-09-13 13:07:32 -0400

    fix: set pageSize to default value if excluded

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -2,17 +2,16 @@ import {useState} from 'react'
 
 const Slideshow = props => {
     const slides = props.slides
+    const onSort = props.onSort || ''
+    const pageSize = props.pageSize || 1
     const btnClasses = "btn btn-primary"
     const btnDisabled = btnClasses + " disabled"
     const [currPage, setCurrSlide] = useState(0)
-    const currIndex = currPage * props.pageSize
-    const numPages = Math.ceil(slides.length / props.pageSize)
-    if (!props.onSort) {
-        props.onSort = ''
-    }
+    const currIndex = currPage * pageSize
+    const numPages = Math.ceil(slides.length / pageSize)
     return (
         <div className="slides-app">
-            {(numPages ? numPages : '') && slides.slice(currIndex, currIndex + props.pageSize)}
+            {(numPages ? numPages : '') && slides.slice(currIndex, currIndex + pageSize)}
             <div style={{display: 'flex', justifyContent: 'center', marginTop: '1rem'}}>
                 <div className="btn-group">
                     <button
@@ -27,9 +26,9 @@ const Slideshow = props => {
                         className={currPage < numPages - 1 ? btnClasses : btnDisabled}
                         onClick={() => setCurrSlide(currPage + 1)}
                     >Next</button>
-                    {props.onSort && <button
+                    {onSort && <button
                         className={btnClasses}
-                        onClick={props.onSort}
+                        onClick={onSort}
                     >{props.sortText ? props.sortText : props.sortBy ? `Sort by ${props.sortBy}` : 'Sort'}</button>}
                 </div>
             </div>
